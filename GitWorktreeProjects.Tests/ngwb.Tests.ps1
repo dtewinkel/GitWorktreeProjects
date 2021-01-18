96e8de275384781d8e37bb4729e6dfc1e239f0ba
Daniël te Winkel <bitbucket@te-winkel.nl>
2021-01-18 21:09:32 +0100

    Tested and fixed alias.

---
+++ GitWorktreeProjects.Tests/ngwb.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe "ngwb" {
+
+	It "should be an alias of New-GitWorktreeBranch" {
+
+		. $PSScriptRoot/Helpers/LoadModule.ps1
+
+		$alias = Get-Alias ngwb
+
+		$alias.ReferencedCommand | Should -Be "New-GitWorktreeBranch"
+	}
+}
--- GitWorktreeProjects/New-GitWorktreeBranch.ps1
+++ GitWorktreeProjects/New-GitWorktreeBranch.ps1
@@ -14,4 +14,4 @@
 	}
 }
 
-New-alias -Name ngwb Open-GitWorktreeBranch
+New-alias -Name ngwb New-GitWorktreeBranch
