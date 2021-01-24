a73225ca379763dc2f595cfdf5a94d214676a77a
Daniël te Winkel <bitbucket@te-winkel.nl>
2021-01-24 13:27:56 +0100

    Make sure alias is exported, and test the alias.

---
+++ GitWorktreeProjects.Tests/rgwb.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe "rgwb" {
+
+	It "should be an alias of Remove-GitWorktreeBranch" {
+
+		. $PSScriptRoot/Helpers/LoadModule.ps1
+
+		$alias = Get-Alias rgwb
+
+		$alias.ReferencedCommand | Should -Be "Remove-GitWorktreeBranch"
+	}
+}
