56ebe11c644289b5056dc5c2379263ed29779b64
Daniël te Winkel <bitbucket@te-winkel.nl>
2021-02-01 21:25:51 +0100

    Allow environment setting for alternate path to config files.

--- GitWorktreeProjects/Config/GetProjectConfig.ps1
+++ GitWorktreeProjects/Config/GetProjectConfig.ps1
@@ -9,11 +9,17 @@ function GetProjectConfig
 
 	process
 	{
-		$configFilePath = Join-Path -Path ${HOME} -ChildPath .gitworktree -AdditionalChildPath "${Project}.project"
-		if (-not (Test-Path $configFilePath))
+		$projectConfigFileName = "${Project}.project"
+		$configFilePath = $env:GitWorktreeConfigPath
+		if(-not $configFilePath)
 		{
-			throw "Project Config File '${configFilePath}' for project '${Project}' not found! Use New-GitWorktreeProject to create it."
+			$configFilePath = Join-Path -Path ${HOME} -ChildPath .gitworktree
 		}
-		[ProjectConfig](Get-Content -Path $configFilePath | ConvertFrom-Json)
+		$configFile = Join-Path -Path $configFilePath -ChildPath $projectConfigFileName
+		if (-not (Test-Path $configFile))
+		{
+			throw "Project Config File '${configFile}' for project '${Project}' not found! Use New-GitWorktreeProject to create it."
+		}
+		[ProjectConfig](Get-Content -Path $configFile | ConvertFrom-Json)
 	}
 }
