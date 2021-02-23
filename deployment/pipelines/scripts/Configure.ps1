f5bb068e4f5b3f1c4d9307f2186dc246d7f10108
Daniël te Winkel <daniel@twia.nl>
2021-02-23 13:05:03 +0100

    Fixed setup of modules.

--- deployment/pipelines/scripts/Configure.ps1
+++ deployment/pipelines/scripts/Configure.ps1
@@ -11,35 +11,47 @@ $reportGenerator = Join-Path $toolsPath reportgenerator.exe
 	$moduleSpec = $_ -split '@'
 	switch ($moduleSpec.Length)
 	{
-		1
-		{
+		1 {
 			$getParams = @{
 				Name = $moduleSpec[0]
 			}
 			$installParams = @{
 				Name = $moduleSpec[0]
 			}
+			$importParams = @{
+				Name = $moduleSpec[0]
+			}
 		}
-		
-		2
-		{
+
+		2 {
 			$getParams = @{
 				FullyQualifiedName = @{
-					ModuleName    = $moduleSpec[0]
+					ModuleName = $moduleSpec[0];
 					ModuleVersion = $moduleSpec[1]
 				}
 			}
 			$installParams = @{
-				Name           = $moduleSpec[0]
+				Name = $moduleSpec[0];
 				MinimumVersion = $moduleSpec[1]
 			}
+			$importParams = @{
+				Name = $moduleSpec[0];
+				MinimumVersion = $moduleSpec[1]
+				Scope = 'Local'
+			}
 		}
+
+		Default {
+			throw "Illegal module spec '$moduleSpec'. Can only contain one @."
+		}
+
 	}
-	$module = Get-Module @getParams
-	if (-not $module)
+	$moduleDefinition = Get-Module @getParams -ListAvailable
+	if (-not $moduleDefinition)
 	{
-		Install-Module @installParams -Scope CurrentUser -Force -PassThru
+		Install-Module @installParams -Scope CurrentUser -Force -Verbose:$false
 	}
+	Import-Module @importParams
 } | Format-Table -AutoSize
 
 if (-not (Test-Path $reportGenerator))
