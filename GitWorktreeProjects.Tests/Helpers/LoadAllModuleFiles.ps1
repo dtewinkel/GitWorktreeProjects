e2fe40e1d03321721b76aa015caf6f89c0ca3a1f
Daniël te Winkel <daniel@twia.nl>
2021-05-01 21:19:10 +0200

    Added unit tests for ProjectArgumentCompleter.

---
+++ GitWorktreeProjects.Tests/Helpers/LoadAllModuleFiles.ps1
@@ -0,0 +1,6 @@
+﻿$modulePath = (Resolve-Path (Join-Path $PSScriptRoot '..' '..', "Modules", "GitWorktreeProjects")).Path
+
+Get-ChildItem (Join-Path $modulePath Types *.ps1) | ForEach-Object FullName | Resolve-Path | ForEach-Object { . $_ }
+Get-ChildItem (Join-Path $modulePath Config *.ps1) | ForEach-Object FullName | Resolve-Path | ForEach-Object { . $_ }
+Get-ChildItem (Join-Path $modulePath Tools *.ps1) | ForEach-Object FullName | Resolve-Path | ForEach-Object { . $_ }
+Get-ChildItem (Join-Path $modulePath ArgumentCompleters *.ps1) | ForEach-Object FullName | Resolve-Path | ForEach-Object { . $_ }
---
+++ GitWorktreeProjects.Tests/ProjectArgumentCompleter.Tests.ps1
@@ -0,0 +1,54 @@
+Describe "ProjectArgumentCompleter" {
+
+	BeforeAll {
+		Push-Location
+		. $PSScriptRoot/Helpers/LoadAllModuleFiles.ps1
+		. $PSScriptRoot/Helpers/LoadModule.ps1
+		. $PSScriptRoot/Helpers/BackupGitWorktreeConfigPath.ps1
+	}
+
+	It "should have the right parameters" {
+		$command = Get-Command ProjectArgumentCompleter
+		$command | Should -HaveParameter wordToComplete
+	}
+
+	It "should expand to nothing when no projects" {
+		. $PSScriptRoot/Helpers/SetGitWorktreeConfig.ps1 -Scope Custom -Setup NoProjects
+		$result = ProjectArgumentCompleter -wordToComplete ""
+		$result | Should -BeNullOrEmpty
+	}
+
+	It "should expand ProjectFilter to nothing with 3 projects" {
+		. $PSScriptRoot/Helpers/SetGitWorktreeConfig.ps1 -Scope Custom -Setup ThreeProjects
+
+		$result = ProjectArgumentCompleter -wordToComplete ""
+		$result | Should -HaveCount 3
+		$result[0].CompletionText | Should -Be "AnotherProject"
+		$result[0].ListItemText | Should -Be "AnotherProject"
+		$result[0].ResultType | Should -Be "ParameterValue"
+		$result[0].ToolTip | Should -BeLike "Project AnotherProject in*"
+		$result[1].CompletionText | Should -Be "MyFirstProject"
+		$result[1].ListItemText | Should -Be "MyFirstProject"
+		$result[1].ResultType | Should -Be "ParameterValue"
+		$result[1].ToolTip | Should -BeLike "Project MyFirstProject in*"
+		$result[2].CompletionText | Should -Be "SecondProject"
+		$result[2].ListItemText | Should -Be "SecondProject"
+		$result[2].ResultType | Should -Be "ParameterValue"
+		$result[2].ToolTip | Should -BeLike "Project SecondProject in*"
+	}
+
+	It "should expand ProjectFilter to nothing with 3 projects" {
+		. $PSScriptRoot/Helpers/SetGitWorktreeConfig.ps1 -Scope Custom -Setup ThreeProjects
+		$result = ProjectArgumentCompleter -wordToComplete "Se"
+		$result | Should -HaveCount 1
+		$result[0].CompletionText | Should -Be "SecondProject"
+		$result[0].ListItemText | Should -Be "SecondProject"
+		$result[0].ResultType | Should -Be "ParameterValue"
+		$result[0].ToolTip | Should -BeLike "Project SecondProject in*"
+	}
+
+	AfterAll {
+		. $PSScriptRoot/Helpers/RestoreGitWorktreeConfigPath.ps1
+		Pop-Location
+	}
+}
