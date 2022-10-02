f03d549c8def775f4b9fff886698fbb9ddf71f77
Daniël te Winkel <daniel@twia.nl>
2022-10-02 19:34:37 +0200

    Completed tests for Get-GitWorktree.

--- GitWorktreeProjects.Tests/Get-GitWorktree.Tests.ps1
+++ GitWorktreeProjects.Tests/Get-GitWorktree.Tests.ps1
@@ -11,47 +11,98 @@ Describe "Get-GitWorktree" {
 		Push-Location
 		. $PSScriptRoot/Helpers/LoadModule.ps1 -ModuleFolder $ModuleFolder
 
-		$projectName = "Testing123"
-		$worktrees = @(
-			@{
-				Name = "Test123"
-			}
-			@{
-				Name = "Demo123"
-			}
-		)
-
-		$projectConfig = @{
-			SchemaVersion = 1
-			Name          = $projectName
-			Worktrees     = $worktrees
-		}
+		$projectConfigs = & $PSScriptRoot/Helpers/GetTestProjectConfig.ps1
+		$projectConfig = $projectConfigs[0]
 	}
 
 	It "should have the right parameters" {
 		$command = Get-Command Get-GitWorktree
 
-		$command | Should -HaveParameter WorktreeFilter -HasArgumentCompleter
-		$command | Should -HaveParameter Project -HasArgumentCompleter
+		$command | Should -HaveParameter Project -HasArgumentCompleter -DefaultValue '.' -Type [string]
+		$command | Should -HaveParameter WorktreeFilter -HasArgumentCompleter -DefaultValue '*' -Alias 'Filter' -Type [string]
 	}
 
-	It "should return all worktrees for the currenct project if no parameters are given" {
+	It "should return all working trees for the project if a project is specified" {
 
+		$projectFilter = 'Testing123'
+		Mock GetProjects { @( $projectFilter ) } -ModuleName GitWorktreeProjects `
+			-ParameterFilter { $Filter -eq $projectFilter } -Verifiable
+		Mock GetProjectConfig { $projectConfig } -ModuleName GitWorktreeProjects `
+			-ParameterFilter { $Project -eq $projectFilter -and $WorktreeFilter -eq '*' } -Verifiable
+
+		$worktrees = Get-GitWorktree -Project $projectFilter
+
+		Should -InvokeVerifiable
+		$worktrees | Should -HaveCount 3
+		$worktrees[0].Name | Should -Be "main"
+		$worktrees[1].Name | Should -Be "Test123"
+		$worktrees[2].Name | Should -Be "Demo123"
+	}
+
+	It "should return all working trees for the currenct project if -Project is '.'" {
+
+		Mock GetProjects { } -ModuleName GitWorktreeProjects
+		Mock GetProjectConfig { $projectConfig } -ModuleName GitWorktreeProjects `
+			-ParameterFilter { $Project -eq '.' -and $WorktreeFilter -eq '*' } -Verifiable
+
+		$worktrees = Get-GitWorktree -Project '.'
+
+		Should -InvokeVerifiable
+		Should -Not -Invoke GetProjects -ModuleName GitWorktreeProjects
+		$worktrees | Should -HaveCount 3
+		$worktrees[0].Name | Should -Be "main"
+		$worktrees[1].Name | Should -Be "Test123"
+		$worktrees[2].Name | Should -Be "Demo123"
+	}
+
+	It "should fail if -Project is not '.' and no project can be found" {
+
+		$projectName = 'Test*'
+		Mock GetProjects { } -ModuleName GitWorktreeProjects `
+			-ParameterFilter { $Filter -eq $projectName } -Verifiable
+		Mock GetProjectConfig { } -ModuleName GitWorktreeProjects
+
+		{ Get-GitWorktree -Project $projectName } | Should -Throw "No project found with name ${projectName}. Make sure the name is correct."
+
+		Should -InvokeVerifiable
+		Should -Not -Invoke GetProjectConfig -ModuleName GitWorktreeProjects
+	}
+
+	It "should fail if -Project is not '.' and more than one project is found" {
+
+		$projectName = 'Test*'
+		Mock GetProjects { $projectConfigs } -ModuleName GitWorktreeProjects `
+			-ParameterFilter { $Filter -eq $projectName } -Verifiable
+		Mock GetProjectConfig { } -ModuleName GitWorktreeProjects
+
+		{ Get-GitWorktree -Project $projectName } | Should -Throw "Multiple projects found that match name ${projectName}. Make sure the name is correct and that no wildcards are used that match more than one project."
+
+		Should -InvokeVerifiable
+		Should -Not -Invoke GetProjectConfig -ModuleName GitWorktreeProjects
+	}
+
+	It "should return all working trees for the currenct project if no parameters are given" {
+
+		Mock GetProjects { } -ModuleName GitWorktreeProjects
 		Mock GetProjectConfig { $projectConfig } -ModuleName GitWorktreeProjects `
 			-ParameterFilter { $Project -eq '.' -and $WorktreeFilter -eq '*' } -Verifiable
 
 		$worktrees = Get-GitWorktree
 
 		Should -InvokeVerifiable
-		$worktrees | Should -HaveCount 2
-		$worktrees[0].Name | Should -Be "Test123"
-		$worktrees[1].Name | Should -Be "Demo123"
+		Should -Not -Invoke GetProjects -ModuleName GitWorktreeProjects
+		$worktrees | Should -HaveCount 3
+		$worktrees[0].Name | Should -Be "main"
+		$worktrees[1].Name | Should -Be "Test123"
+		$worktrees[2].Name | Should -Be "Demo123"
 	}
 
-	It "should return all worktrees for the project and worktrees for which the parameters are given" {
+	It "should return all working trees for the project and working trees for which the parameters are given" {
 
 		$projectFilter = 'Testing123'
 		$worktreeFilter = '*123'
+		$projectConfig.Worktrees = $projectConfig.Worktrees[1..2]
+
 		Mock GetProjects { @( $projectFilter ) } -ModuleName GitWorktreeProjects `
 			-ParameterFilter { $Filter -eq $projectFilter } -Verifiable
 		Mock GetProjectConfig { $projectConfig } -ModuleName GitWorktreeProjects `
@@ -65,11 +116,12 @@ Describe "Get-GitWorktree" {
 		$worktrees[1].Name | Should -Be "Demo123"
 	}
 
-	It "should return an empty array if no match for the project and worktrees for which the parameters are given" {
+	It "should return an empty array if no match for the project and working trees combination for which the parameters are given" {
 
 		$projectFilter = 'Testing123'
 		$worktreeFilter = 'None'
 		$projectConfig.Worktrees = @()
+
 		Mock GetProjects { @( $projectFilter ) } -ModuleName GitWorktreeProjects `
 			-ParameterFilter { $Filter -eq $projectFilter } -Verifiable
 		Mock GetProjectConfig { $projectConfig } -ModuleName GitWorktreeProjects `
@@ -80,31 +132,5 @@ Describe "Get-GitWorktree" {
 		Should -InvokeVerifiable
 		$worktrees.GetType() | Should -Be 'Worktree[]'
 		$worktrees | Should -BeNullOrEmpty
-	}
-
-	It "should fail if -Project is not '.' and no project can be found" {
-
-		Mock GetProjectConfig { } -ModuleName GitWorktreeProjects
-		Mock GetProjects { } -ModuleName GitWorktreeProjects `
-			-ParameterFilter { $Filter -eq $projectName } -Verifiable
-		$projectName = 'Test*'
-
-		{ Get-GitWorktree -Project $projectName } | Should -Throw "No project found with name ${projectName}. Make sure the name is correct and that no wildcards are used that match more than one project."
-
-		Should -InvokeVerifiable
-		Should -Not -Invoke GetProjectConfig -ModuleName GitWorktreeProjects
-	}
-
-	It "should fail if -Project is not '.' and more than one project is found" {
-
-		Mock GetProjectConfig { } -ModuleName GitWorktreeProjects
-		Mock GetProjects { @(1, 2) } -ModuleName GitWorktreeProjects `
-			-ParameterFilter { $Filter -eq $projectName } -Verifiable
-		$projectName = 'Test*'
-
-		{ Get-GitWorktree -Project $projectName } | Should -Throw "No project found with name ${projectName}. Make sure the name is correct and that no wildcards are used that match more than one project."
-
-		Should -InvokeVerifiable
-		Should -Not -Invoke GetProjectConfig -ModuleName GitWorktreeProjects
 	}
 }
--- GitWorktreeProjects/Get-GitWorktree.ps1
+++ GitWorktreeProjects/Get-GitWorktree.ps1
@@ -2,15 +2,15 @@
 {
 <#
 	.SYNOPSIS
-		Get the worktrees for a GitWorktree project.
+		Get information about the working trees for a GitWorktree project.
 
 	.DESCRIPTION
-		Get some or all worktrees for a GitWorktree project.
+		Get information about some or all working trees for a GitWorktree project.
 
-		By default Get-GitWorktree gets all worktrees for the current project.
-		Optionally a project can be specified with -Project.
+		By default Get-GitWorktree gets information about all working trees for the
+		current project. Optionally a project can be specified with -Project.
 
-		The set of worktrees can be filtered with the -WorktreeFilter parameter.
+		The set of working trees can be filtered with the -WorktreeFilter parameter.
 		For the fitering the same wildcards can be used as for the -like operator.
 
 		Tab completion is supported for the -Project and the -WorktreeFilter parameters.
@@ -24,22 +24,27 @@
 	.EXAMPLE
 		Get-GitWorktree
 
-		Return all worktrees for the current project.
+		Returns information about all working trees for the current project.
 
 	.EXAMPLE
 		Get-GitWorktree -Project Demo -WorktreeFilter ma*
 
-		Return all worktrees where the name starts with 'ma' for the project 'Demo'.
+		Returns information about all working trees where the name starts with 'ma'
+		for the project 'Demo'.
 
 	.EXAMPLE
 		Get-GitWorktree . feature/*
 
-		Return all worktrees for feature branches for the current project.
+		Returns information about all working trees for feature branches for the
+		current project.
 
 	.LINK
 		about_Wildcards
 
-	.LINK
+		New-GitWorktree
+		Open-GitWorktree
+		Remove-GitWorktree
+
 		Get-GitWorktreeProject
 #>
 
@@ -47,49 +52,62 @@
 	[OutputType([Worktree[]])]
 	param(
 		<#
-		The project to get the worktrees for.
+		The project to get the information about the working trees for.
 
-		Either specify the exact name of a project, or use wildcards to filter for a specific project. Exactly one project name must match the wildcard.
+		Either specify the exact name of a project, or use wildcards to filter for a
+		specific project. Exactly one project name must match the wildcard.
 		For the fitering the same wildcards can be used as for the -like operator.
-		If the project name does not exist, or the wilcard does not match exactly one project, then this will throw an error.
+		If the project name does not exist, or the wilcard does not match exactly one
+		project, then this will throw an error.
 
 		The special project name '.' may be used to refer to the current project.
-		This can be used when called from anywhere inside the folder structure of the current project.
-		If project name '.' is used outside of the folder structure of the current project then this will throw an error.
+		This can be used when called from anywhere inside the folder structure of
+		the current project.
+		If project name '.' is used outside of the folder structure of the current
+		project then this will throw an error.
 
 		Defaults to '.' to get the current project.
 
 		Supports tab completion to select the project.
 		#>
 		[Parameter()]
-		[ArgumentCompleter({ _gwp__ProjectArgumentCompleter @args })]
+		[ArgumentCompleter({ _gwp__ProjectArgumentCompleter -WordToCompete $args[2] })]
 		[String] $Project = '.',
 
 		<#
-		Filter to select the worktrees names for the project.
+		Filter to select the working tree names for the project.
 
-		Either specify the exact name of a worktree, or use wildcards to filter for a specific worktree or set of
-		worktrees. For the fitering the same wildcards can be used as for the -like operator.
+		Either specify the exact name of a working tree, or use wildcards to filter
+		for a specific working tree or set of working trees. For the fitering the
+		same wildcards can be used as for the -like operator.
 
-		Defaults to '*' to get all worktrees.
+		Defaults to '*' to get all working trees.
 
-		Supports tab competion to select a worktree for the project.
+		Supports tab competion to select a working tree for the project.
 		#>
 		[Parameter()]
 		[Alias("Filter")]
-		[ArgumentCompleter({ _gwp__worktreeArgumentCompleter @args })]
+		[ArgumentCompleter({ _gwp__worktreeArgumentCompleter -WordToCompete $args[2] -FakeBoundParameters $args[4] })]
 		[String] $WorktreeFilter = '*'
 	)
 
 	if ($Project -ne '.')
 	{
 		$projects = @(GetProjects $Project)
-		if (-not $projects -or $projects.Length -ne 1)
+
+		if (-not $projects)
 		{
-			throw "No project found with name ${Project}. Make sure the name is correct and that no wildcards are used that match more than one project."
+			throw "No project found with name ${Project}. Make sure the name is correct."
+		}
+
+		if ($projects.Count -ne 1)
+		{
+			throw "Multiple projects found that match name ${Project}. Make sure the name is correct and that no wildcards are used that match more than one project."
 		}
 	}
+
 	$projectConfig = GetProjectConfig -Project $Project -WorktreeFilter $WorktreeFilter -FailOnMissing
+
 	Write-Output ([Worktree[]]($projectConfig.Worktrees) ?? @()) -NoEnumerate
 }
 
